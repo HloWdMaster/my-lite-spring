fbabd5a7840295508bb94899b7a5cf14cf6611e5
HloWdMaster <1457391646@qq.com>
2021-01-08 13:27:35 +0800

    IOC-property属性强转RuntimeBeanReference和TypedStringValue

---
+++ src/test/java/org/litespring/test/v2/V2AllTest.java
@@ -0,0 +1,16 @@
+package org.litespring.test.v2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.litespring.test.v1.ApplicationContextTest;
+import org.litespring.test.v1.BeanFactoryTest;
+import org.litespring.test.v1.ResourceTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ApplicationContextTestV2.class,
+        BeanDefinitionTestV2.class,
+        BeanDefinitionValueResolverTest.class
+})
+public class V2AllTest {
+}
