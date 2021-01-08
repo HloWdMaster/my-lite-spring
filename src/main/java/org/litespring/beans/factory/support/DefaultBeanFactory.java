c13d3fe3c466a2c831813f9529a5f3fe15320426
HloWdMaster <1457391646@qq.com>
2021-01-08 18:46:43 +0800

    IOC-使用common-beanutils进行属性填充

--- src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
+++ src/main/java/org/litespring/beans/factory/support/DefaultBeanFactory.java
@@ -1,5 +1,6 @@
 package org.litespring.beans.factory.support;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.litespring.beans.BeanDefinition;
 import org.litespring.beans.PropertyValue;
 import org.litespring.beans.SimpleTypeConverter;
@@ -55,8 +56,29 @@ public class DefaultBeanFactory extends DefaultSingletonBeanRegistry
         //创建实例
         Object bean = instantiateBean(bd);
         //设置属性
-        populateBean(bd, bean);
+//        populateBean(bd, bean);
+        //
+        populateBeanUserCommBeanUtils(bd, bean);
         return bean;
+    }
+
+    private void populateBeanUserCommBeanUtils(BeanDefinition bd, Object bean) {
+        List<PropertyValue> pvs = bd.getPropertyValues();
+        if (pvs == null || pvs.isEmpty()) {
+            return;
+        }
+        BeanDefinitionValueResolver valueResolver = new BeanDefinitionValueResolver(this);
+        try {
+            for (PropertyValue pv : pvs) {
+                String propertyName = pv.getName();
+                Object originalValue = pv.getValue();
+                Object resolvedValue = valueResolver.resolveValueIfNecessary(originalValue);
+                BeanUtils.setProperty(bean,propertyName,resolvedValue);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new BeanCreationException("Failed to obtain BeanInfo for class [" + bd.getBeanClassName() + "]");
+        }
     }
 
     protected void populateBean(BeanDefinition bd, Object bean) {
